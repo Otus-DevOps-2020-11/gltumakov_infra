86ad34060aba02f7340af827bc11e0fbb3e8b8a8
Hlib Tumakov <gl.tumakov@gmail.com>
2021-01-08 18:39:18 +0200

    Create packer dir, add scripts and files dirs, move *.sh files into scripts/ dir, create puma.service in files/ dir, add ubuntu16.json and immutable.json files to packer/ dir, add variables.json.example file, create config-reddit-vm.sh script in config-scripts/ dir, add .gitignore file, update metadata.yml and README.md

---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get install -y git
+
+cd /usr/local
+
+git clone -b monolith https://github.com/express42/reddit.git
+
+cd reddit && bundle install
+
+mv /tmp/puma.service /etc/systemd/system/puma.service
+
+systemctl daemon-reload
+systemctl start puma.service
+systemctl enable puma.service
+echo 'Application started and listening on port 9292...'
+
+echo '------------------'
